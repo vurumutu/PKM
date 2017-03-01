c585491feae1ac861cc880a526e884287953ec61
Karol Trzcinski <k.trzcinski995@gmail.com>
2017-03-01 22:06:35 +0100

    Dodanie balisy

--- CAN.py
+++ CAN.py
@@ -1,4 +1,5 @@
 import serial
+import time
 import CAN_const
 
 # ramka:
@@ -9,63 +10,79 @@ import CAN_const
 # n: parameters
 # 1: '\r'
 
+
 class Agent:
     address = 0
+
     def send(self, data):
         ser.write('>'+format(self.address, "x")+' '+data+"\r")
+
+    def read(self, cmd):
+        ser.flushInput()
+        self.send(cmd)
+        time.sleep(1)
+        return ser.read_all()
+
+    def skanuj(self):
+        ser.flushInput()
+        self.send("1F000000 39")
+        time.sleep(1)
+        return ser.read_all()
+
+    def zmien_adres(self, strefa, adres):
+        ser.write("61 "+format(strefa, 'x')+format(adres, 'x'))
+        self.address = strefa << 16 + adres
 
     class Semafora:
         def wlacz(self, led):
             self.send(format(led, "x") + " 00")
+
         def mrugaj(self, led):
             self.send("00 " + format(led, "x"))
+
         def wylacz(self):
-            self.send(format(LED_OFF, "x") + ' ' + format(LED_OFF, "x"))
+            self.send(format(0x30, "x") + ' ' + format(0x30, "x"))
+
 
     class Zwrotnica:
         def lewo(self):
             self.send(format("31", "x"))
+
         def prawo(self):
             self.send(format("32", "x"))
+
         def wylacz(self):
             self.send(format("30", "x"))
 
     class Balisa:
-
-
+        def wlacz(self, histereza):
+            self.send("33 "+format(histereza, "x"))
 
 
 # configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
-    port='/dev/ttyUSB1',
+    #port='/dev/ttyUSB1',
+    #port='COM3',
+    0,
     baudrate=500,
     parity=serial.PARITY_ODD,
     stopbits=serial.STOPBITS_TWO,
     bytesize=serial.SEVENBITS
 )
 
-ser.isOpen()
-ser.write('master\r')
-print 'Enter your commands below.\r\nInsert "exit" to leave the application.'
+if ser.isOpen():
+    print("Serial is open: ")
+else:
+    print("Serial is closed!!")
+print(ser.portstr)
+ser.write(format("master\r"))
 
-input=1
-while 1 :
-    # get keyboard input
-    input = raw_input(">> ")
-        # Python 3 users
-        # input = input(">> ")
-    if input == 'exit':
-        ser.close()
-        exit()
-    else:
-        # send the character to the device
-        # (note that I happend a \r\n carriage return and line feed to the characters - this is requested by my device)
-        ser.write(input + '\r\n')
-        out = ''
-        # let's wait one second before reading output (let's give device time to answer)
-        time.sleep(1)
-        while ser.inWaiting() > 0:
-            out += ser.read(1)
-
-        if out != '':
-            print ">>" + out
+if __name__ == '__main__':
+    a1 = Agent
+    print(a1.skanuj(a1))
+    while 1:
+        a1.Zwrotnica.lewo()
+        time.sleep(5)
+        a1.Zwrotnica.prawo()
+        time.sleep(5)
+    ser.close()
