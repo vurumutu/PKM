a5a354ac610a5285e72aa9b27e6fea329e44b0b5
Pawel <pawelrogasz@gmail.com>
2017-03-12 00:16:23 +0100

    doadanie suwaka prędkości

--- window.py
+++ window.py
@@ -14,17 +14,32 @@ class Window(QtGui.QMainWindow):
         btn1 = QtGui.QPushButton("Sprawdz pozycje", self)
         btn1.clicked.connect(self.close_application)
         btn1.resize(btn1.minimumSizeHint())
-        btn1.move(100, self.height()-30)
+        btn1.move(10, self.height()-30)
 
         btn2 = QtGui.QPushButton("Ustaw pociagi", self)
         btn2.clicked.connect(self.close_application)
         btn2.resize(btn2.minimumSizeHint())
-        btn2.move(10, self.height()-30)
+        btn2.move(120, self.height()-30)
+
+        self.create_slider(210,self.height()-50)
 
         self.show()
 
+    def create_slider(self, x, y):
+        l1 = QtGui.QLabel("Predkosc", self)
+        l1.setAlignment(QtCore.Qt.AlignCenter)
+        l1.move(x, y)
+
+        slider_speed = QtGui.QSlider(QtCore.Qt.Horizontal, self)
+        slider_speed.setMinimum(10)
+        slider_speed.setMaximum(30)
+        slider_speed.setValue(20)
+        slider_speed.setTickPosition(QtGui.QSlider.TicksBelow)
+        slider_speed.setTickInterval(5)
+        slider_speed.move(x, y+20)
+
     def check_position(self):
-        print()
+        print("test")
 
     def close_application(self):
         sys.exit()
