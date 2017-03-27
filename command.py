c4d1039477996e931c84d3573c42a8d89b162b47
Bartosz Żbikowski <zbiku112@op.pl>
2017-03-27 09:12:44 +0200

    Usunięcie niepotrzebnej komendy

--- command.py
+++ command.py
@@ -48,30 +48,7 @@ class Train(object):
     # Tworzenie komendy do sterowania pociagiem
     def move(self, velocity, course=0):
         command = self.header(4) + self.address(self.locomotive) + self.speed(velocity, course)
-        xor = self.xor(command)
-        command += xor
         return command
-
-    # Tworzenie xora do komendy
-    def xor(self, command):
-        parts = []
-        xor = ''
-        while command:
-            parts.append(int(command[:2], 16))
-            command = command[2:]
-        for i in range(1, len(parts)):
-            if i == 1:
-                xor = parts[i - 1] ^ parts[i]
-            else:
-                xor ^= parts[i]
-        if len(parts) == 1:
-            xor = parts[0]
-        if xor < 16:
-            xor = hex(xor)[2:]
-            xor = '0' + xor
-        else:
-            xor = hex(xor)[2:]
-        return xor
 
     @staticmethod
     def menu():
