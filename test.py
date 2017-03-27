8e4708033b58e19862653923d3ac7963743ba8bc
Bartosz Żbikowski <zbiku112@op.pl>
2017-03-27 08:53:58 +0200

    Dodanie obsługi błędów, zmiana komendy sterowania

--- test.py
+++ test.py
@@ -21,18 +21,22 @@ def main():
     # TODO Rozbudowa menu do testów
     while True:
         numer = input("Podaj numer pociągu[1-7]: ")
+        if not numer in range(1, 8):
+            # cls()
+            print("Nieprawidlowy numer pociagu.\n")
+            continue
         train = Train(numer)
         while True:
             key = input(train.menu())
             if key == 1:
-                msg = train.move(13, direction["Forward"])  # do przodu z prędkością max
+                msg = train.move(127, direction["Forward"])  # do przodu z prędkością max
             elif key == 2:
-                msg = train.move(13, direction["Backward"])  # do tylu z prędkością max
+                msg = train.move(127, direction["Backward"])  # do tylu z prędkością max
             elif key == 3:
-                vel = input("Zakres[0-13]: ")
+                vel = input("Zakres[0-127]: ")
                 msg = train.move(vel, direction["Forward"])  # do przodu z określoną prędkością
             elif key == 4:
-                vel = input("Zakres[0-13]: ")
+                vel = input("Zakres[0-127]: ")
                 msg = train.move(vel, direction["Backward"])  # do tylu z określoną prędkością
             elif key == 5:
                 msg = train.move(0)  # Stop
