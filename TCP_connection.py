25a52bcb63fad11a6bf7914e28c23c3f1166a95c
Bartosz Żbikowski <zbiku112@op.pl>
2017-03-27 08:55:51 +0200

    Zmiana kodu odpowiadającego za wysyłanie, drobne zmiany w kodzie

--- TCP_connection.py
+++ TCP_connection.py
@@ -6,7 +6,7 @@ import socket
 import threading
 from datetime import datetime
 from time import sleep
-
+import binascii
 
 class Client(object):
     # Klasa do obsługi połączenia z sterownikiem
@@ -15,24 +15,22 @@ class Client(object):
         self.connected = False
         self.address = ''
         self.port = ''
+        self.lock = threading.Lock()
         pass
 
-    def __keepAlive__(self):
-        # TODO Prawdopodobnie po testach z kodowaniem niepotrzebna metoda
-        while self.connected:
-            self.connection.send('21 21 00')
-            sleep(5)
-            cc = ''
-            for c in self.connection.recv(512):
-                if len(hex(int(ord(c)))) == 1:
-                    cc += '0'
-                cc += hex(int(ord(c)))
-            print 'Received: ' + cc.replace('0x', ' ')
-
-    def __start_reader__(self):
-        self.receiver_thread = threading.Thread(target=self.__keepAlive__)
-        self.receiver_thread.setDaemon(True)
-        self.receiver_thread.start()
+    def keep_alive(self, once=False):
+        while True:
+            self.lock.acquire()
+            self.connection.setblocking(False)
+            try:
+                msg = self.receive()
+            except:
+                pass
+            self.connection.setblocking(True)
+            self.lock.release()
+            if once:
+                break
+            sleep(0.2)
 
     def connect(self, address, port):
         self.address = address
@@ -44,7 +42,7 @@ class Client(object):
                 self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.connection.connect((self.address, self.port))
                 self.connected = True
-                self.__start_reader__()
+                threading._start_new_thread(self.keep_alive, False)
                 print str(datetime.now().strftime('%H:%M:%S')) + ": Połączono z sterownikiem"
 
             except socket.error as message:
@@ -60,9 +58,17 @@ class Client(object):
 
     def send(self, message):
         # Wysłanie wiadomości do hosta
+        self.keep_alive(True)
         if self.connected:
+            message = 'fffe' + message
+            message = binascii.unhexlify(message)
+            self.lock.acquire()
             self.connection.send(message)
+            self.lock.release()
             print str(datetime.now().strftime('%H:%M:%S')) + ": Wysłano wiadomość: " + str(message)
-            # TODO Metoda ma zwracać odebraną wiadomość na podstawie wysłanej wiadomości (Dodanie nowej metody receive)
+            # TODO Metoda ma zwracać odebraną wiadomość na podstawie wysłanej wiadomości
 
+     #TODO Dodanie nowej metody receive)
+    def receive(self):
+        return "Not added yet."
     # TODO Dodanie metod do obsługi poszczególnych rozkazów do sterownika nie związanych z obsługą pociągów
