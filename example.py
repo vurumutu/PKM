f34db9abc52ca49d22c0d355c89354ebd9c07186
ontsidec <franek93paw@gmail.com>
2017-04-02 22:43:25 +0200

    Poprawki i scalenie w jeden plik

--- example.py
+++ example.py
@@ -1,33 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from TCP_connection import Client
-from command import Train
+from xpressnet import Client
+from xpressnet import Train
 from time import sleep
-import os
 
 TCP_IP = '192.168.210.200'
 TCP_PORT = 5550
 
 
-def cls():
-    # TODO Sprawdzić działanie w konsoli - czyszczenie ekranu w obsłudze menu
-    os.system('cls' if os.name == 'nt' else 'clear')
+def menu():
+    print("1. Do przodu - max prędkość")
+    print("2. Do tylu - max prędkość")
+    print("3. Do przodu - ustal prędkość")
+    print("4. Do tylu - ustal prędkość")
+    print("5. Stop - zerowa prędkość")
+    print("6. Wyłącz zasilanie pociągów")  # klawisz off na pilocie
+    print("0. Zakończ")
+    print("Aby zakończyć program naciśnij klawisz q.")
 
 
 def main():
     direction = {"Forward": 1, "Backward": 0}
     client = Client()
     client.connect(TCP_IP, TCP_PORT)
-    # TODO Rozbudowa menu do testów
     while True:
         numer = input("Podaj numer pociągu[1-7]: ")
-        if not numer in range(1, 8):
-            # cls()
-            print("Nieprawidlowy numer pociagu.\n")
+        if numer not in range(1, 8):
+            print("Nieprawidłowy numer pociągu.\n")
             continue
         train = Train(numer)
         while True:
-            key = input(train.menu())
+            key = input(menu())
             if key == 1:
                 msg = train.move(127, direction["Forward"])  # do przodu z prędkością max
             elif key == 2:
@@ -39,17 +42,18 @@ def main():
                 vel = input("Zakres[0-127]: ")
                 msg = train.move(vel, direction["Backward"])  # do tylu z określoną prędkością
             elif key == 5:
-                msg = train.move(0)  # Stop
+                msg = train.move(0)  # Stop - zadanie zerowej prędkości
             elif key == 6:
-                msg = train.stop_all_locomotives()
+                msg = client.stop_all_locomotives()  # Zatrzymanie wszystkich lokomotyw awaryjnie
+            elif key == 7:
+                msg = train.stop_locomotive()  # Zatrzymanie aktualnej lokomotywy awaryjnie
             elif key == 0:
                 msg = train.move(0)  # Zakończ
                 client.send(msg)
                 sleep(1)
                 break
             else:
-                # cls()
-                print("Podano nieprawidłowy klawisz. Cofam na poczatek.")
+                print("Podano nieprawidłowy klawisz!")
                 continue
             client.send(msg)
             sleep(1)  # Czekaj 1s
@@ -59,11 +63,6 @@ def main():
         else:
             continue
     client.disconnect()
-
-
-def menu():
-    print("Aby zakończyć program naciśnij klawisz q.")
-
 
 if __name__ == "__main__":
     main()
