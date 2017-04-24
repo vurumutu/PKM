d7e71f9496adf42ea586592bca33361555d17a2a
pawstern95 <sternicki.pawel1995@gmail.com>
2017-04-24 23:08:05 +0200

    Aktualizacja wyznaczania modelu

--- pomiary/pomiary.py
+++ pomiary/pomiary.py
@@ -1,6 +1,7 @@
+from math import *
 import numpy as np
-from matplotlib import pyplot as plt
-from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+from scipy.optimize import fmin
 
 time = []
 
@@ -21,11 +22,42 @@ velocity = 20/time  # cm per second
 velocity = np.insert(velocity, 0, 0)
 
 
-z = np.polyfit(t, velocity, 3)
+def step_response(t, T1, T2):
+    return 1 - (T1/(T1-T2))*e**(-t/T1) + (T2/(T1-T2))*e**(-t/T2)
 
+
+def impulse_response(t, T1, T2):
+    return (e**(-t/T1) - e**(-t/T2)) * (1/(T1-T2))
+
+
+def my_fmin(param, time, y):
+    cost = 0
+    for i, t in enumerate(time):
+        try:
+            cost += (y[i] - h_prim(t, param[0], param[1], param[2], param[3])) ** 2
+        except:
+            cost += inf
+    return cost
+
+
+def h_prim(t, T1, T2, tau_z, k):
+    return k * (tau_z * impulse_response(t, T1, T2) + step_response(t, T1, T2))
+
+# aproksymacja do wielomianu
+z = np.polyfit(t, velocity, 4)
 p = np.poly1d(z)
 xp = np.linspace(min(t), (max(t)), 200)
-y = p(xp)
-print(y)
-plt.plot(t, velocity, '.', xp, p(xp), 'g')
+yp = p(xp)
+
+result = fmin(func=lambda params: my_fmin(params, t, velocity), x0=[10, 5, 0.4, 30])
+
+y_count = []
+for time_1 in t:
+    y_count.append(h_prim(time_1, *result))
+print("       k              T1          T2              Tz")
+print(result[3], result[0], result[1], result[2])
+plt.plot(t, velocity, 'r.')
+plt.plot(t, y_count)
+#plt.plot(xp, yp, 'bx')
+plt.title("Odpowiedz skokowa")
 plt.show()
