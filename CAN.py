6b05477124f7955aafb468d820cb53d5b4ac2ac2
Karol Dworakowski <kardwor1@student.pg.gda.pl>
2017-05-14 15:04:10 +0200

    cleanup, poprawka do dodawania bez powtorzen

--- CAN.py
+++ CAN.py
@@ -18,7 +18,7 @@ STR_Rebiechowo = 0x03
 STR_Banino = 0x04
 STR_Wrzeszcz = 0x05
 
-#semafory
+# semafory
 LED_GREEN = 0x31
 LED_YELLOW1 = 0x32
 LED_RED = 0x33
@@ -47,7 +47,7 @@ class Agent:
         self.l_address = l_addr
 
     def send(self, data):
-        msg = '>'+self.address+' ' + data + '\r'
+        msg = '>' + self.address + ' ' + data + '\r'
         ser.write(msg.decode('unicode-escape'))
 
     @staticmethod
@@ -55,7 +55,7 @@ class Agent:
         ser.write(u'>1F000000 39\r')
 
     def zmien_adres(self, strefa, adres):
-        ser.write("61 "+format(strefa, 'x')+format(adres, 'x'))
+        ser.write('61 ' + strefa + adres)
         self.address = strefa << 16 + adres
 
 
@@ -64,13 +64,13 @@ class Semafor:
         self.agent = Agent(addr, strefa, l_addr)
 
     def wlacz(self, led):
-        self.agent.send(format(led, "x") + " 00")
+        self.agent.send(led + ' 00')
 
     def mrugaj(self, led):
-        self.agent.send("00 " + format(led, "x"))
+        self.agent.send('00 ' + led)
 
     def wylacz(self):
-        self.agent.send(format(0x30, "x") + ' ' + format(0x30, "x"))
+        self.agent.send('0x30 ' + '0x30')
 
 
 class Zwrotnica:
@@ -80,13 +80,13 @@ class Zwrotnica:
         self.limiter = limit
 
     def lewo(self):
-        self.agent.send(format("31", "x"))
+        self.agent.send('31')
 
     def prawo(self):
-        self.agent.send(format("32", "x"))
+        self.agent.send('32')
 
     def wylacz(self):
-        self.agent.send(format("30", "x"))
+        self.agent.send('30')
 
 
 class Balisa:
@@ -97,11 +97,10 @@ class Balisa:
 
     def wlacz(self, hist):
         self.histereza = hist
-        self.agent.send("33 " + format(self.histereza, "x"))
+        self.agent.send('33 ' + self.histereza)
 
     def wylacz(self):
-        self.agent.send("30")
-
+        self.agent.send('30')
 
 ### KONIEC KLAS ###
 def _readline():
@@ -113,63 +112,88 @@ def _readline():
         else:
             ms.append(c)
 
+
 def can_odb():
     while ser_raw.isOpen():
         reading = ser.readline()
-        #print('odebralem: '+reading)
+        # print('odebralem: '+reading)
         handle_data(reading)
     print('can_odb koniec')
+
+
+def sprawdz(items, key):  # czy mamy juz taki adres w tablicy
+    for item in items:
+        if item.agent.address == key:
+            return item
+    return None
 
 
 time1 = 0
 last_time1 = 0
 
-# funkcja parsujaca pojedyncza komende/odpowiedz
 def handle_scan(data):
     global time1
     global last_time1
 
+    address = data[1:9]
     typ = data[1:3]
     strefa = data[3:5]
     l_adres = data[5:9]
     attr1 = data[10:12]
     attr2 = data[13:15]
+    attr3 = data[16:18]
+    attr4 = data[19:21]
 
-    if typ == '01' and len(zwrotnica) < 49:
-    # do mierzenia czasu
-        # for x in zwrotnica:
-        #     if x.l_addr == l_adres:
-        #         return None
-        zwrotnica.append(Zwrotnica(data[1:9], strefa, l_adres, attr1, attr2))
+    if typ == '01':
+        zwr = sprawdz(zwrotnica, address)
+        if zwr:
+            zwr.stat = attr1
+            zwr.limiter = attr2
+        else:
+            zwrotnica.append(Zwrotnica(address, strefa, l_adres, attr1, attr3))
 
-    elif typ == '02' and len(semafor) < 49: #Semafor
-        semafor.append(Semafor(data[1:9], strefa, l_adres))
-    elif typ == '03' and len(balisa) < 49: # Balisa
-        # for x in balisa:
-        #     if x.l_addr == l_adres:
-        #         print 'balisa ' + l_adres + '\t' #+ end - start
-        #         return None
-        #         last_time = time
-        #         time = timeit.timeit()
-        #         print time-last_time
-        balisa.append(Balisa(data[1:9], strefa, l_adres, attr1, attr2))
-    if typ == '03' and data[16:18] <'70': # wieksze tyl mniejsze przod
-        #print data[16:18]
-        # print u'time1 ' + str(time1)
-        # print u'last_time ' + str(last_time1)
-        last_time1 = time1
-        time1 = time.clock() # aktualny czas
-        print u'balisa ' + l_adres + u'\t' + str(time1 - last_time1)
-        #print (time1 - last_time1)
+    elif typ == '02':  # Semafor
+        sem = sprawdz(semafor, address)
+        if not sem:
+            semafor.append(Semafor(address, strefa, l_adres))
+
+    elif typ == '03':  # Balisa
+        bal = sprawdz(balisa, address)
+        if bal:
+            bal.state = attr1
+            bal.histereza = attr2
+            if attr3 < '70':  # wieksze tyl mniejsze przod dla pociagow 1 i 2
+                last_time1 = time1
+                time1 = time.clock()  # aktualny czas
+                print u'balisa ' + l_adres + u'\t' + str(time1 - last_time1)
+        else:
+            balisa.append(Balisa(address, strefa, l_adres, attr1, attr2))
+
+            # doGUI(address)
+            # for x in balisa:
+            #     if x.l_addr == l_adres:
+            #         print 'balisa ' + l_adres + '\t' #+ end - start
+            #         return None
+            #         last_time = time
+            #         time = timeit.timeit()
+            #         print time-last_time
+            # if typ == '03' and attr3 < '70': # wieksze tyl mniejsze przod
+            # print data[16:18]
+            # print u'time1 ' + str(time1)
+            # print u'last_time ' + str(last_time1)
+            # print (time1 - last_time1)
 
 
-# obsluga ciagu wejsciowego z CAN
 def handle_data(data):
     dat = data.split('\r')
     for d in dat:
         handle_scan(d)
 
-#configure the serial connections (the parameters differs on the device you are connecting to)
+        # f = open('pkm_scan.txt', 'r')
+        # data = f.read()
+        # print data
+
+
 ser_raw = serial.Serial(
     port='COM3',
     # port='COM21',
@@ -180,61 +204,25 @@ ser_raw = serial.Serial(
 )
 
 if ser_raw.isOpen():
-    print("Serial is open: ")
+    print('Serial is open: ')
 else:
-    print("Serial is closed!!")
+    print('Serial is closed!!')
 print(ser_raw.portstr)
 
-# aby obejsc domyslne konczenie ramki znakiem '\n' i konczyc ja '\r'
 ser = io.TextIOWrapper(io.BufferedRWPair(ser_raw, ser_raw, 1),
                        newline='\r',
                        line_buffering=True)
-ser._CHUNK_SIZE = 1 # pojedyncza wielkosc bufora odbiorczego, aby nie wprowadzac opoznien
-
-# listy agentow
-zwrotnica = []
-balisa = []
-semafor = []
+ser._CHUNK_SIZE = 1
 
 # watek odbioru
 watek_odb = threading.Thread(target=can_odb)
 watek_odb.start()
 
-'''
-if __name__ == '__main__':
-    a1 = Agent()
-    z1 = Zwrotnica(a1)
-    print(a1.skanuj())
-    for i in range(5):
-        z1.lewo()
-        time.sleep(5)
-        z1.prawo()
-        time.sleep(5)
-    ser.close()
-    '''
-
 ser.write(u'master\r')
 Agent.skanuj()
 
-
 time.sleep(1)
-for balisas in balisa: # ustawiamy automatyczne zglaszanie i histereze
+for balisas in balisa:  # ustawiamy automatyczne zglaszanie i histereze
     balisas.wlacz(0x70)
-    time.sleep(0.1) # bez sleepa zapycha sie
+    time.sleep(0.5)  # bez sleepa zapycha sie
 
-print "test balis"
-print len(zwrotnica)
-print len(zwrotnica)
-print len(balisa)
-
-# time.sleep(1)
-# Agent.skanuj()
-
-# f = open('pkm_scan.txt', 'r')
-# data = f.read()
-# print data
-
-#while True:
-    #time.sleep(5)
-    # print('suma: ' + str(len(balisa)+len(zwrotnica)+len(semafor)))
-    #break
