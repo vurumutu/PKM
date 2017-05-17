f258c6655617983b85b7eed720cfaf5d0481eb74
ontsidec <franek93paw@gmail.com>
2017-05-17 11:08:08 +0200

    poprawki po mergu

--- CAN.py
+++ CAN.py
@@ -1,8 +1,5 @@
 import serial
 import time
-<<<<<<< HEAD
-import CAN_const
-=======
 import timeit
 import threading
 import io
@@ -31,7 +28,6 @@ LED_YELLOW2 = 0x34
 LED_WHITE = 0x35
 LED_OFF = 0x30
 LED_GET = 0x39
->>>>>>> CAN_devel
 
 # ramka:
 # 1: '>'
@@ -41,8 +37,6 @@ LED_GET = 0x39
 # n: parameters
 # 1: '\r'
 
-<<<<<<< HEAD
-=======
 __builtin__.adres = 0
 __builtin__.test1 = 0
 __builtin__.trasa_idx = [0, 0, 0, 0]
@@ -59,90 +53,26 @@ trasa3 = ['0304012E', '0304012D'] # wrzeszcz -> banino
 trasa4 = ['0304012D', '0304012F'] # banino -> wrzeszcz
 
 
->>>>>>> CAN_devel
 
 class Agent:
-    address = 0
+    def __init__(self, addr='1F000000', strefa='', l_addr=None):
+        self.address = addr
+        self.strefa = strefa
+        self.l_address = l_addr
 
     def send(self, data):
-<<<<<<< HEAD
-        ser.write('>'+format(self.address, "x")+' '+data+"\r")
-
-    def read(self, cmd):
-        ser.flushInput()
-        self.send(cmd)
-        time.sleep(1)
-        return ser.read_all()
-=======
         msg = '>' + str(self.address) + ' ' + data + '\r'
         ser.write(msg.decode('unicode-escape'))
->>>>>>> CAN_devel
 
-    def skanuj(self):
-        ser.flushInput()
-        self.send("1F000000 39")
-        time.sleep(1)
-        return ser.read_all()
+    @staticmethod
+    def skanuj():
+        ser.write(u'>1F000000 39\r')
 
     def zmien_adres(self, strefa, adres):
         ser.write('61 ' + strefa + adres)
         self.address = strefa << 16 + adres
 
-    class Semafora:
-        def wlacz(self, led):
-            self.send(format(led, "x") + " 00")
 
-<<<<<<< HEAD
-        def mrugaj(self, led):
-            self.send("00 " + format(led, "x"))
-
-        def wylacz(self):
-            self.send(format(0x30, "x") + ' ' + format(0x30, "x"))
-
-
-    class Zwrotnica:
-        def lewo(self):
-            self.send(format("31", "x"))
-
-        def prawo(self):
-            self.send(format("32", "x"))
-
-        def wylacz(self):
-            self.send(format("30", "x"))
-
-    class Balisa:
-        def wlacz(self, histereza):
-            self.send("33 "+format(histereza, "x"))
-
-
-# configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    #port='/dev/ttyUSB1',
-    #port='COM3',
-    0,
-    baudrate=500,
-    parity=serial.PARITY_ODD,
-    stopbits=serial.STOPBITS_TWO,
-    bytesize=serial.SEVENBITS
-)
-
-if ser.isOpen():
-    print("Serial is open: ")
-else:
-    print("Serial is closed!!")
-print(ser.portstr)
-ser.write(format("master\r"))
-
-if __name__ == '__main__':
-    a1 = Agent
-    print(a1.skanuj(a1))
-    while 1:
-        a1.Zwrotnica.lewo()
-        time.sleep(5)
-        a1.Zwrotnica.prawo()
-        time.sleep(5)
-    ser.close()
-=======
 class Semafor:
     def __init__(self, addr, strefa, l_addr):
         self.agent = Agent(addr, strefa, l_addr)
@@ -343,4 +273,3 @@ time.sleep(5)
 for balisas in balisa:  # ustawiamy automatyczne zglaszanie i histereze
     balisas.wlacz(0x70)
     time.sleep(0.5)  # bez sleepa zapycha sie
->>>>>>> CAN_devel
