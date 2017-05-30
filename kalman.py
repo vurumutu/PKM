18ced8c2ffbe6eb679bad02bc3a527bc9f586d46
ontsidec <franek93paw@gmail.com>
2017-05-30 23:23:17 +0200

    chyba dobrze teraz

--- kalman.py
+++ kalman.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 
 
 class Model:
     """Klasa zawierająca modele poszczególnych pociągów"""
+
     def __init__(self, train_nr):
         """Domyślne ustawienia klasy
 
@@ -19,185 +19,159 @@ class Model:
                          [0.],  # v
                          [0.]],  # x
                         dtype=float)
-        self.P = np.mat([[1., 0., 0.],  # initial wariance
+        # initial variance
+        self.P = np.mat([[1., 0., 0.],
                          [0., 1., 0.],
                          [0., 0., 1.]], dtype=float)
 
-        # Modele pociągów
+        # MODELE POCIĄGÓW
+        # Pociąg 1 do PRZODU
         if train_nr == 11:
-            """
-            self.A = np.mat([[-1.141, -0.4455, 0],
-                             [0.5, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.0615384615384615],  # [0.44603076],
-                             [0],
-                             [0.]], dtype=float)
-            self.C = np.mat([[0., 0., 4.067],
-                             [0., 4.067, 0.]], dtype=float)
-            """
-            self.A = np.mat([[-1.782, -0.7939, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.0084904058413992],  # [0.44603076],
+            self.A = np.mat([[-1.382, -0.7939, 0],
+                             [1, 0, 0],
+                             [0, 1, 0]], dtype=float)
+            self.B = np.mat([[0.344904058413992],
                              [0],
                              [0.]], dtype=float)
             self.C = np.mat([[0., 0., 1.],
                              [0., 1., 0.]], dtype=float)
-
+        # Pociąg 1 do TYLU
         if train_nr == 12:
-            self.A = np.mat([[-1.684, -0.7092, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.0096319395114199],  # [0.44603076],
+            self.A = np.mat([[-1.382, -0.7939, 0],
+                             [1, 0, 0],
+                             [0, 1, 0]], dtype=float)
+            self.B = np.mat([[0.344904058413992],
                              [0],
                              [0.]], dtype=float)
             self.C = np.mat([[0., 0., 1.],
                              [0., 1., 0.]], dtype=float)
+        # Pociąg 2 do PRZODU
         if train_nr == 21:
-            self.A = np.mat([[-7.422, -3.443, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.010564542753384],  # [0.44603076],
+            self.A = np.mat([[-0.083, -1.186, 0],
+                             [1, 0, 0],
+                             [0, 1, 0]], dtype=float)
+            self.B = np.mat([[0.41996617639522],
                              [0],
                              [0.]], dtype=float)
             self.C = np.mat([[0., 0., 1.],
                              [0., 1., 0.]], dtype=float)
-
+        # Pociąg 2 do TYLU
         if train_nr == 22:
             self.A = np.mat([[-0.083, -1.186, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.41996617639522],  # [0.44603076],
+                             [1, 0, 0],
+                             [0, 1, 0]], dtype=float)
+            self.B = np.mat([[0.41996617639522],
                              [0],
                              [0.]], dtype=float)
             self.C = np.mat([[0., 0., 1.],
                              [0., 1., 0.]], dtype=float)
-            """
-            self.A = np.mat([[-8.483, -4.486, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.0081996617639522],  # [0.44603076],
-                             [0],
-                             [0.]], dtype=float)
-            self.C = np.mat([[0., 0., 1.],
-                             [0., 1., 0.]], dtype=float)
-            """
+        # Pociąg 5
         if train_nr == 5:
-            self.A = np.mat([[-2.67, -1.783, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.0175874425660079],  # [0.44603076],
+            self.A = np.mat([[-0.11, -9.783, 0],
+                             [1, 0, 0],
+                             [0, 1, 0]], dtype=float)
+            self.B = np.mat([[4.2975874425660079],
                              [0],
                              [0.]], dtype=float)
             self.C = np.mat([[0., 0., 1.],
                              [0., 1., 0.]], dtype=float)
-
+        # Pociąg 6
         if train_nr == 6:
-            self.A = np.mat([[-2.152, -1.158, 0],
-                             [1, 0, 0],  # Prędkość
-                             [0, 1, 0]], dtype=float)  # Droga
-            self.B = np.mat([[0.0128930361488501],  # [0.44603076],
+            self.A = np.mat([[-0.11, -9.783, 0],
+                             [1, 0, 0],
+                             [0, 1, 0]], dtype=float)
+            self.B = np.mat([[2.3385874425660079],
                              [0],
                              [0.]], dtype=float)
             self.C = np.mat([[0., 0., 1.],
                              [0., 1., 0.]], dtype=float)
 
-
-        # variances
-        self.Q = np.mat([[50., 0., 0.],  # process variance
+        # VARIANCES
+        # process variance
+        self.Q = np.mat([[50., 0., 0.],
                          [0., 70.0, 0.],
                          [0., 0., 3500.]], dtype=float)
-        self.R = np.mat([[3.1, 0.],  # sensor variance
+        # sensor variance
+        self.R = np.mat([[3.1, 0.],
                          [0., 30.1]], dtype=float)
 
-        self.motorPower = 65.0
-        self.stopPower = 0.
+        self.motorPower = 0
         self.updateTime = time.time()
         self.updatePosition = 0.
         self.simulateTime = time.time()
 
-    def simulatesym(self, time, updatestate=True):
-        """ podaj czas z przyszlosci do ktorego mam symulowac pociag
-        jezeli updatestate bedzie false, to zmienne pociagu zostana zachowane"""
+    def simulate(self, update_state=True):
+        """Zwraca pozycje pociągu
+
+             Args:
+                update_state (bool): Jeżeli True to symuluje model dla aktualnego czasu i aktualizuje go, czyli
+                                     generalnie zawsze kiedy chcemy sprawdzić aktualną pozycję w danym momencie.
+
+        """
+        self.simulate_T(time.time(), update_state)
+
+    def simulate_T(self, time, update_state=True):
+        """Zwraca pozycje pociągu
+
+             Args:
+                time (float): Przyszły czas do którego ma być symulowany model
+                update_state (bool): Jeżeli True to symuluje model dla aktualnego czasu i aktualizuje go, czyli
+                                     generalnie zawsze kiedy chcemy sprawdzić aktualną pozycję w danym momencie.
+
+        """
         if time < self.simulateTime - self.dT:
             print(time, " < ", self.simulateTime)
             raise Exception('Nie cofaj czasu!')
         X, P, t = np.copy(self.X), np.copy(self.P), self.simulateTime
+
         while t < time:
             t += self.dT
             X += (self.A * X + self.B * self.motorPower) * self.dT
         P = self.A * P * self.A.transpose() + self.Q * (t - self.updateTime)
-        if updatestate:
+
+        if update_state:
             self.X = X
-            # self.P = P
             self.simulateTime = t
         return X, P
 
-    def simulate(self, updatestate=True):
-        self.simulatesym(time.time(), updatestate)
+    def get_stop_distance(self):
+        """Zwraca dystans jaki pociąg jeszcze przejedzie jeżeli zacznie się zatrzymywać"""
 
-    def predict(self, time):
-        """parametrem jest aktualny czas, zwracana jest macierz,
-        nie mozna cofac czasu, domyslnie nie aktualizuje zmiennych pociagu"""
-        X, P = self.simulatesym(time, updatestate=False)
-        return self.C * X
-
-    def get_stop_distance(self, update_time):
-        """
-                # TODO Jaki czas podać tutaj? Skąd go przypisać?
-                actual_time = time.time()
-                if actual_time < self.simulateTime - self.dT:
-                    print(time, " < ", self.simulateTime)
-                    raise Exception('Nie cofaj czasu!')
-                X, P, t = np.copy(self.X), np.copy(self.P), self.simulateTime
-                # Robimy symulacje bez zapisywania
-                while t < actual_time:
-                    t += self.dT
-                    X += (self.A * X + self.B * self.motorPower) * self.dT
-                # Licz dopóki prędkość jest większa równa od zera
-                while (self.C * self.X)[1, 0] >= 0:
-                    t += self.dT
-                    X += (self.A * X + self.B * self.stopPower) * self.dT
-
-                return (self.C * X)[0, 0] - (self.C * self.X)[0, 0]
-                """
-        """ zwraca dystanc potrzebny do wychamowania. W centymetrach.
-        """
-        self.simulatesym(update_time, updatestate=True)  # zasymuluj do aktualnej chwili czasu
+        self.simulate()  # zasymuluj do aktualnej chwili czasu
         power, time, state, P = self.motorPower, self.simulateTime, np.copy(self.X), np.copy(self.P)
         v = self.get_velocity(False)
         self.motorPower = 0  # moc na silniku podczas hamowania
-        while v * self.get_velocity(False) > 0.0001:  # > 0, czyli gdy paciag przed i po symulacji jedzie w tym samym kierunku,
-            # pamietajmy o cofaniu,
+
+        while v * self.get_velocity(False) > 0.0001:
             time += self.dT
-            self.simulatesym(time, updatestate=True)  # aktualizujemy stan w celach optymalizacyjnych
-        pozycja = self.get_position(False)  # zapisz pozycje gdzie sie pociag zatrzymal
-        self.motorPower, self.simulateTime, self.X, self.P = power, time, state, P  # przywroc oryginalny stan
-        print(pozycja)
-        print(self.get_position(False))
+            self.simulate_T(time, update_state=True)  # aktualizujemy stan w celach optymalizacyjnych
+
+        pozycja = self.get_position(False)  # zapisz pozycje gdzie sie pociąg zatrzymał
+        self.motorPower, self.simulateTime, self.X, self.P = power, time, state, P  # przywróć oryginalny stan
+
+        print("Początek zatrzymania: ", self.get_position(False))
+        print("Koniec zatrzymania: ", pozycja)
         return pozycja - self.get_position(False)
 
     def update(self, distance):
-        """ aktualizuje system wzgledem aktualnego czasu
-        i przejechanej odleglosci jako parametr"""
-        dt = time.time() - self.updateTime
-        self.updatesym(distance, dt)
+        """Aktualizuje model względem aktualnego czasu i przejechanej odległości.
 
-    def updatesym(self, distance, dt, freshstate=False):
-        """podaj przejechana odleglosc od ostatniej aktualizacji
-        oraz czas jaki minal na jej przejechanie"""
-        Xpom = np.mat([[self.updatePosition + distance],
-                       [distance / dt]])
+             Args:
+                distance (float): Przebyta droga od ostatniego update lub startu
+
+        """
+        dt = time.time() - self.updateTime
+        Xpom = np.mat([[self.updatePosition + distance], [distance / dt]])
+
         # predykcja
-        if not freshstate:
-            Xprio, Pprio = self.simulatesym(self.updateTime + dt, False)
-        else:
-            Xprio, Pprio = self.X, self.P
+        Xprio, Pprio = self.simulate_T(self.updateTime + dt, False)
         Pprio = self.A * self.P * self.A.transpose() + self.Q * dt
+
         # korekcja
         e = Xpom - self.C * Xprio
         temp = self.C * Pprio * self.C.transpose() + self.R * dt
         K = Pprio * self.C.transpose() * np.linalg.inv(temp)
+
         # aktualizacja
         I = np.eye(self.A.shape[0])
         self.X = Xprio + K * e
@@ -209,7 +183,8 @@ class Model:
         """Zwraca pozycje pociągu
 
              Args:
-                simulate (bool): Jeżeli True to symuluje model dla aktualnego czasu i aktualizuje go
+                simulate (bool): Jeżeli True to symuluje model dla aktualnego czasu i aktualizuje go, czyli generalnie
+                                 zawsze kiedy chcemy sprawdzić aktualną pozycję w danym momencie.
 
              Returns:
                 (float): Zwraca pozycje pociągu (dystans od startu)
@@ -223,7 +198,8 @@ class Model:
         """Zwraca prędkość pociągu
 
              Args:
-                simulate (bool): Jeżeli True to symuluje model dla aktualnego czasu i aktualizuje go
+                simulate (bool): Jeżeli True to symuluje model dla aktualnego czasu i aktualizuje go, czyli generalnie
+                                 zawsze kiedy chcemy sprawdzić aktualną pozycję w danym momencie.
 
              Returns:
                 (float): Zwraca prędkość pociągu
@@ -240,79 +216,18 @@ class Model:
                  power (float): moc od 0.0 - 127.0
 
          """
-        self.motorPower = power
+        if power >= 127:
+            self.motorPower = 127
+        elif power <= 0:
+            self.motorPower = 0
+        else:
+            self.motorPower = power
 
 
+# TESTOWANIE
 if __name__ == "__main__":
-    distances = [203, 230, 624, 272]
-    times = [18.1354904881, 27.5069689903, 53.9750874511, 7.80411009911]
-    times = [7.08, 6.68, 17.55, 7.9]
-    t1 = Model(22)
-    t = Model(11)
-
+    t = Model(6)
+    t.set_power(65)
     # dla symulacji ustaw czas na 0
-    t.updateTime = 0
-    t.simulateTime = 0
-    t1.updateTime = 0
-    t1.simulateTime = 0
-    print(t1.get_stop_distance(t1.simulateTime + 10.))
-    print("Pozycja: ", t1.get_position(False))
-
-    # zmienne do wyswietlania
-    tim = [0.]
-    timpred = [0.]
-    X = [0.]
-    V = [0.]
-    pom = [0.]
-    pred = [0.]
-    vpom = [0.]
-    vpred = [0.]
-    var = [0.]
-
-    # symuluj przejazd
-    for i in range(len(distances)):
-        temptime = 0
-        while temptime < times[i]:
-            x, p = t.simulatesym(t.simulateTime + t.dT, updatestate=True)
-            pred.append(t.get_position(False))
-            vpred.append(t.get_velocity(False))
-            timpred.append(t.simulateTime)
-            temptime += t.dT
-        t.updatesym(distances[i], times[i], freshstate=True)
-        X.append(t.get_position(False))
-        V.append(t.get_velocity(False))
-        var.append(t.P[1, 1])
-
-        pom.append(pom[-1] + distances[i])
-        vpom.append(distances[i] / times[i])
-        tim.append(tim[-1] + times[i])
-
-    # symuluj dodatkowe kilka probek
-    temptime = 0
-    while temptime < 3:
-        x, p = t.simulatesym(t.simulateTime + t.dT, updatestate=True)
-        pred.append(t.get_position(False))
-        vpred.append(t.get_velocity(False))
-        timpred.append(t.simulateTime)
-        temptime += t.dT
-
-    # wyswietl wyniki
-    plt.figure(1)
-    plt.plot(tim, X, 'x', label='filtr')
-    plt.plot(tim, pom, 'o', label='pomiar')
-    plt.plot(timpred, pred, label='predykcja')
-    plt.legend(loc='best')
-    plt.title('droga')
-
-    plt.figure(2)
-    plt.plot(tim, V, 'x', label='filtr')
-    plt.plot(tim, vpom, 'o', label='pomiar')
-    plt.plot(timpred, vpred, label='predykcja')
-    plt.legend(loc='best')
-    plt.title('predkosc')
-
-    # plt.figure(3)
-    # plt.plot(tim, var)
-    # plt.title('wariancja predkosci')
-
-    # plt.show()
+    time.sleep(10)  # Poruszamy pociągiem przez 10 sekund
+    print("Dystans hamowania: ", t.get_stop_distance())
