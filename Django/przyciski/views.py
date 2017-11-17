4c6195abed0bb00becc047dd45e0109d9b9935c3
vurumutu <wojciech.zgliniecki@gmail.com>
2017-11-17 17:13:05 +0100

    Dodana prosta strona HTML

--- Django/przyciski/views.py
+++ Django/przyciski/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def index (request):
+    return render_to_response('przyciski/webpage/stronka.html')
